22cd9908d86ea51f4d7918b0a8662798ad3fa934
Kel_Seduc <kelabrantes.me@gmail.com>
2025-03-07 13:03:21 -0300

    up github

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="flex justify-center h-screen">
+      <div className=" flex flex-col px-3 max-w-[880px]">
+        <div className="flex flex-col items-center mt-36 gap-3">
+          <h1 className="text-5xl font-medium lg:text-8xl">ConvertDocx</h1>
+          <p className="italic lg:text-xl">Converta seu pdf em .docx</p>
+        </div>
+        <div className="flex flex-col lg:flex-row gap-2 justify-center mt-8">
+          <Input className="cursor-pointer" type="file"/>
+          <Button className="cursor-pointer">Converter</Button>
+        </div>
+      </div>
+      <footer className="absolute bottom-0 text-xs flex flex-col items-center w-full font-[400] text-gray-500 mb-4 lg:text-base  ">
+          <p>&copy; ConvertDocx</p>
+          <p>Criado por <Link href="https://x.com/Kel_Abr" className="text-blue-600">Kel_abr</Link></p>
+        </footer>
+     
     </main>
   );
 }
